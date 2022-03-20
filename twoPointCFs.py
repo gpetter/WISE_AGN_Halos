949eb7d6723b82db60189e53a54a4b7a32f091f3
graysonpetter <graysonpetter@gmail.com>
2022-03-20 15:10:52 -0400

    Added angular cross-correlation

--- twoPointCFs.py
+++ twoPointCFs.py
@@ -4,8 +4,6 @@ from Corrfunc.mocks.DDtheta_mocks import DDtheta_mocks
 from Corrfunc.utils import convert_3d_counts_to_cf
 import myCorrfunc
 import numpy as np
-import importlib
-importlib.reload(myCorrfunc)
 
 def data_counts(scales, ras, decs, weights, fulldict=True):
 	dd = DDtheta_mocks(1, 1, scales, ras, decs, weights1=weights)
@@ -63,31 +61,53 @@ def angular_corr_from_coords(ras, decs, randras, randdecs, scales, weights=None,
 
 
 # angular cross correlation
-def ang_cross_corr_from_coords(ras, decs, refras, refdecs, randras, randdecs, minscale, maxscale, weights=None,
-                               refweights=None, randweights=None, nthreads=1, nbins=10):
+def ang_cross_corr_from_coords(ras, decs, refras, refdecs, randras, randdecs, minscale, maxscale, refrandras=None,
+                               refranddecs=None,  weights=None,
+                               refweights=None, randweights=None, refrandweights=None, nthreads=1, nbins=10):
+
+	if (refweights is not None) | (weights is not None) | (randweights is not None) | (refrandweights is not None):
+		weighttype = 'pair_product'
+		n_data = np.sum(weights)
+		n_ref = np.sum(refweights)
+		n_rands = np.sum(randweights)
+		n_refrands = np.sum(refrandweights)
+
+	else:
+		weighttype = None
+		n_data = len(ras)
+		n_ref = len(refras)
+		n_rands = len(randras)
+		n_refrands = len(refrandras)
+
+
+
 	# set up logarithimically spaced bins in units of degrees
 	bins = np.logspace(minscale, maxscale, (nbins + 1))
 
 	# count pairs between sample and control sample
-	DD_counts = DDtheta_mocks(0, nthreads, bins, ras, decs, RA2=refras, DEC2=refdecs, weights1=weights,
-	                          weights2=refweights)
+	D1D2_counts = DDtheta_mocks(0, nthreads, bins, ras, decs, RA2=refras, DEC2=refdecs, weights1=weights,
+	                          weights2=refweights, weight_type=weighttype)
 
-	# extract number counts
-	dd = []
-	for j in range(nbins):
-		dd.append(DD_counts[j][3])
+	D2R1_counts = DDtheta_mocks(0, nthreads, bins, refras, refdecs, RA2=randras, DEC2=randdecs, weights1=refweights,
+	                            weights2=randweights)
 
 
-	# cross correlation between sample and random catalog
-	DR_counts = np.array(DDtheta_mocks(0, nthreads, bins, ras, decs, RA2=randras, DEC2=randdecs, weights1=weights,
-							  weights2=randweights))
-	dr = []
-	for j in range(nbins):
-		dr.append(DR_counts[j][3])
+	if refrandras is not None:
+		D1R2_counts = DDtheta_mocks(0, nthreads, bins, ras, decs, RA2=refrandras, DEC2=refranddecs, weights1=weights,
+		                            weights2=refrandweights, weight_type=weighttype)
+		R1R2_counts = DDtheta_mocks(0, nthreads, bins, randras, randdecs, RA2=refrandras, DEC2=refranddecs,
+		                            weights1=randweights, weights2=refrandweights, weight_type=weighttype)
+
+		wtheta = myCorrfunc.convert_counts_to_cf(ND1=n_data, ND2=n_ref, NR1=n_rands, NR2=n_refrands,
+		                                         D1D2=D1D2_counts, D1R2=D1R2_counts, D2R1=D2R1_counts,
+		                                         R1R2=R1R2_counts, estimator='LS')
+	else:
+		wtheta = myCorrfunc.convert_counts_to_cf(ND1=n_data, ND2=None, NR1=n_rands, NR2=None,
+		                                         D1D2=D1D2_counts, D1R2=D2R1_counts, D2R1=D2R1_counts,
+		                                         R1R2=D2R1_counts, estimator='Peebles')
 
 
+	w_poisson_err = (1 + wtheta) / np.sqrt(D1D2_counts['npairs'])
 
-	wtheta = np.array(dd)/np.array(dr) * (float(len(randras))) / float(len(refras)) - 1
-
-	return wtheta
+	return wtheta, w_poisson_err
 
