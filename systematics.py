b123b0043fd833f6cc2bd43c79c22d6368b65624
gpetter <graysonpetter@gmail.com>
2022-06-16 14:35:35 -0400

    added function to control for two systematics simultaneously

--- systematics.py
+++ systematics.py
@@ -7,17 +7,18 @@ import astropy.units as u
 from astropy.table import Table, hstack, vstack
 import importlib
 import pandas as pd
-import plotting
+#import plotting
 from source import coord_transforms
 import healpixhelper
-import matplotlib.pyplot as plt
 from scipy import interpolate
-
+from scipy import stats
 
 
 
 # examine whether
 def density_by_coord(datatab, randtab, coordframe, lonlat, ncoordbins, applyweights=True, quantilebins=None):
+	import time
+	st = time.time()
 
 	if not applyweights:
 		bootrandixs = np.random.choice(len(randtab), int(len(randtab) / 1.5), replace=False,
@@ -25,27 +26,28 @@ def density_by_coord(datatab, randtab, coordframe, lonlat, ncoordbins, applyweig
 		randtab = randtab[bootrandixs]
 
 
-	coords = SkyCoord(datatab['RA'] * u.deg, datatab['DEC'] * u.deg)
-	randcoords = SkyCoord(randtab['RA'] * u.deg, randtab['DEC'] * u.deg)
+	#coords = SkyCoord(datatab['RA'] * u.deg, datatab['DEC'] * u.deg)
+	#randcoords = SkyCoord(randtab['RA'] * u.deg, randtab['DEC'] * u.deg)
 	if coordframe == 'C':
-		lon, lat = coords.ra, coords.dec
-		randlon, randlat = randcoords.ra, randcoords.dec
+		lon, lat = datatab['RA'], datatab['DEC']
+		randlon, randlat = randtab['RA'], randtab['DEC']
 	elif coordframe == 'G':
-		lon, lat = coords.galactic.l, coords.galactic.b
-		randlon, randlat = randcoords.galactic.l, randcoords.galactic.b
+		lon, lat = datatab['l'], datatab['b']
+		randlon, randlat = randtab['l'], randtab['b']
 	elif coordframe == 'E':
-		lon, lat = coords.geocentricmeanecliptic.lon, coords.geocentricmeanecliptic.lat
-		randlon, randlat = randcoords.geocentricmeanecliptic.lon, randcoords.geocentricmeanecliptic.lat
+		lon, lat = datatab['elon'], datatab['elat']
+		randlon, randlat = randtab['elon'], randtab['elat']
 	else:
 		return
 
 
-	norm = float(len(randcoords)) / len(coords)
+
+	norm = float(len(randlon)) / len(lon)
 
 	if lonlat == 'lon':
-		vals, randvals = lon.value, randlon.value
+		vals, randvals = lon, randlon
 	elif lonlat == 'lat':
-		vals, randvals = lat.value, randlat.value
+		vals, randvals = lat, randlat
 	else:
 		return
 
@@ -64,11 +66,17 @@ def density_by_coord(datatab, randtab, coordframe, lonlat, ncoordbins, applyweig
 
 	idxs, randidx = np.digitize(vals, quantilebins), np.digitize(randvals, quantilebins)
 
+	print(time.time() - st)
+
 
 	idxs = idxs[np.where((idxs > 0) & (idxs <= len(bincenters)))]
 	randidx = randidx[np.where((randidx > 0) & (randidx <= len(bincenters)))]
 
+	print(time.time() - st)
+
 	datacounts, randcounts = np.bincount(idxs)[1:], np.bincount(randidx)[1:]
+
+	print(time.time() - st)
 
 
 	ratios = datacounts / randcounts * norm
@@ -77,12 +85,16 @@ def density_by_coord(datatab, randtab, coordframe, lonlat, ncoordbins, applyweig
 	errors = np.sqrt(np.square(norm / randcounts * np.sqrt(datacounts)) + np.square(norm * datacounts / np.square(
 		randcounts) * np.sqrt(randcounts)))
 
+	print(time.time() - st)
+
 
 	if applyweights:
 		ratio_interpolator = interpolate.interp1d(bincenters, ratios, fill_value='extrapolate')
 		interped_ratios, interped_rand_ratios = ratio_interpolator(vals), ratio_interpolator(randvals)
-		datatab['weight'] *= 1. / interped_ratios
-		randtab['weight'] *= interped_rand_ratios
+		datatab['weight'] *= (1. / interped_ratios) ** 2
+		randtab['weight'] *= interped_rand_ratios ** 2
+
+	print(time.time() - st)
 
 
 	#plotting.density_vs_coord(binnum, ratios, errors, bincenters, coordframe, lonlat)
@@ -155,6 +167,7 @@ def density_by_depth(datatab, randtab, depthbins, applyweights=True):
 
 def correct_all_systematics(systnames):
 
+
 	binnedtab = Table.read('catalogs/derived/catwise_binned.fits')
 
 
@@ -166,6 +179,7 @@ def correct_all_systematics(systnames):
 		centers, corcenters, ratios, corratios, errs, corerrs = [], [], [], [], [], []
 		bintab = binnedtab[np.where(binnedtab['bin'] == j+1)]
 		randtab = Table.read('catalogs/derived/ls_randoms_1_filtered.fits')
+
 		if 'RA' in systnames:
 			print('Testing RA')
 			rabins, racenters, raratios, raerrs, bintab, randtab = density_by_coord(bintab, randtab, 'C', lonlat='lon',
@@ -246,13 +260,27 @@ def correct_all_systematics(systnames):
 		                                                                            applyweights=False)
 		corcenters.append(np.log10(corrdepthcenters)), corratios.append(corrdepthratios), corerrs.append(corrdeptherrs)
 
-		bintab.write('catalogs/derived/catwise_binned_%s.fits' % (j+1), format='fits', overwrite=True)
+
 
 		bootrandixs = np.random.choice(len(randtab), int(len(bintab) * 20), replace=False,
 		                               p=randtab['weight'] / np.sum(randtab['weight']))
 		randtab = randtab[bootrandixs]
-		randtab.write('catalogs/derived/catwise_randoms_%s.fits' % (j+1), format='fits', overwrite=True)
+		# just keep positions for clustering and weights for lensing
+		bintab = bintab['RA', 'DEC', 'l', 'b', 'weight']
+		# just keep positions for clustering analysis
+		randtab = randtab['RA', 'DEC']
 
+		try:
+
+			bintab.write('/Volumes/grayson/Type1_2_Halos/catalogs/derived/catwise_binned_%s.fits' % (j + 1),
+			             format='fits', overwrite=True)
+			randtab.write('/Volumes/grayson/Type1_2_Halos/catalogs/derived/catwise_randoms_%s.fits' % (j + 1),
+			              format='fits', overwrite=True)
+			print('saving QSO positions to cluster')
+		except:
+			print('dartFS not mounted, saving to local')
+			bintab.write('catalogs/derived/catwise_binned_%s.fits' % (j+1), format='fits', overwrite=True)
+			randtab.write('catalogs/derived/catwise_randoms_%s.fits' % (j + 1), format='fits', overwrite=True)
 
 
 
@@ -260,6 +288,72 @@ def correct_all_systematics(systnames):
 		                                    systnames)
 
 
+
+
+def elat_and_depth_weights(ncoordbins):
+	binnedtab = Table.read('catalogs/derived/catwise_binned.fits')
+
+	nbins = int(np.max(binnedtab['bin']))
+	randtab = Table.read('catalogs/derived/ls_randoms_1_filtered.fits')
+	randvals = randtab['elat']
+	out, latbins = pd.qcut(randvals, ncoordbins, retbins=True)
+
+	latbins[0] += -0.01
+	latbins[-1] += 0.01
+
+	depth_bins = [0, 1500, 100000]
+	ls_depth_map = hp.read_map('masks/ls_depth.fits')
+	nside = hp.npix2nside(len(ls_depth_map))
+	randdepths = ls_depth_map[hp.ang2pix(nside, randtab['l'], randtab['b'], lonlat=True)]
+
+	randhist = stats.binned_statistic_2d(randvals, randdepths, None, statistic='count',
+	                                     bins=[latbins, depth_bins], expand_binnumbers=True)
+
+	randidxs = np.array(randhist[3])-1
+
+
+
+	for j in range(nbins+1):
+		randtab['weight'] = np.ones(len(randtab))
+		tab = binnedtab[np.where(binnedtab['bin'] == j)]
+		if len(tab) == 0:
+			tab = binnedtab
+		randratio = len(tab)/float(len(randtab))
+
+
+		lats = tab['elat']
+
+		depths = ls_depth_map[hp.ang2pix(nside, tab['l'], tab['b'], lonlat=True)]
+
+
+		thishist = stats.binned_statistic_2d(lats, depths, None, statistic='count',
+		                                     bins=[latbins, depth_bins], expand_binnumbers=True)
+
+		data_idxs = np.array(thishist[3]) - 1
+
+		weight_matrix = 1 / randratio * thishist[0] / randhist[0]
+		weight_matrix[np.where(np.logical_not(np.isfinite(weight_matrix)))] = 1.
+		inv_weight = 1. / weight_matrix
+
+
+
+		randweights = weight_matrix[randidxs[0], randidxs[1]]
+		dataweights = inv_weight[data_idxs[0], data_idxs[1]]
+
+		tab['weight'] = dataweights
+		randtab['weight'] = randweights
+		# just keep positions for clustering and weights for lensing
+		tab = tab['RA', 'DEC', 'l', 'b', 'weight']
+		tab.write('catalogs/derived/catwise_binned_%s.fits' % (j), format='fits', overwrite=True)
+
+		bootrandixs = np.random.choice(len(randtab), int(len(tab) * 20), replace=False,
+		                               p=randtab['weight'] / np.sum(randtab['weight']))
+		thisrandtab = randtab[bootrandixs]
+
+
+		# just keep positions for clustering analysis
+		thisrandtab = thisrandtab['RA', 'DEC']
+		thisrandtab.write('catalogs/derived/catwise_randoms_%s.fits' % (j), format='fits', overwrite=True)
 
 
 
@@ -286,173 +380,6 @@ def smoothed_density(catname, nside):
 
 		plotting.data_vs_random_density(smoothedratio, j+1)
 
-# assign weights to correct for varying optical imaging depth across the field
-def correct_randoms():
-	# read in table of AGN binned by r-W2 color
-	tab = Table.read('catalogs/derived/catwise_binned.fits')
-	randtab = Table.read('catalogs/derived/ls_randoms_1_filtered.fits')
-	nbins = int(np.max(tab['bin']))
-
-
-	# split sample up into two bins of imaging depth
-
-	depthbinedges = [150, 1500, 20000]
-
-	# separate table into regions with shallow and deep optical imaging
-	tab_in_shallow_depth = tab[np.where(depthsforpix < 1500)]
-	tab_in_deep = tab[np.where(depthsforpix > 1500)]
-	randtab_in_shallow_depth = randtab[np.where(randdepthsforpix < 1500)]
-	randtab_in_deep = randtab[np.where(randdepthsforpix > 1500)]
-
-	med_depths = [np.median(np.log10(randtab_in_shallow_depth['PSFDEPTH_R'])),
-	              np.median(np.log10(randtab_in_deep['PSFDEPTH_R']))]
-
-	# coordinates of randoms in deep and shallow footprints
-	randcoords_in_shallow = SkyCoord(randtab_in_shallow_depth['RA'] * u.deg, randtab_in_shallow_depth['DEC'] * u.deg)
-	randcoords_in_deep = SkyCoord(randtab_in_deep['RA'] * u.deg, randtab_in_deep['DEC'] * u.deg)
-
-	# convert RA DEC to other coordinate that presents systematic
-	coordframe = 'E'
-	latlon = 'lat'
-	if coordframe == 'E':
-		shallowrandlon, shallowrandlat = randcoords_in_shallow.geocentricmeanecliptic.lon, \
-		                                 randcoords_in_shallow.geocentricmeanecliptic.lat
-		deeprandlon, deeprandlat = randcoords_in_deep.geocentricmeanecliptic.lon, \
-		                           randcoords_in_deep.geocentricmeanecliptic.lat
-	if latlon == 'lat':
-		shallowrandvals = shallowrandlat
-		deeprandvals = deeprandlat
-	else:
-		shallowrandvals = shallowrandlon
-		deeprandvals = deeprandlon
-
-
-	tmp, deepcoordbins = pd.qcut(deeprandvals, 10, retbins=True)
-
-	tmp, shallowcoordbins = pd.qcut(shallowrandvals, 10, retbins=True)
-	deepbincenters = (deepcoordbins[1:] + deepcoordbins[:-1]) / 2
-	shallowbincenters = (shallowcoordbins[1:] + shallowcoordbins[:-1]) / 2
-	deepcoordbins[0] += -0.001
-	deepcoordbins[-1] += 0.001
-	shallowcoordbins[0] += -0.001
-	shallowcoordbins[-1] += 0.001
-
-	deepfrac = float(len(randtab_in_deep)) / len(randtab)
-	shallowfrac = float(len(randtab_in_shallow_depth)) / len(randtab)
-
-
-
-
-
-
-	# count total number of AGN in each depth bin
-	fullcounts = np.bincount(np.digitize(depthsforpix, depthbinedges))[1:]
-
-
-	avgdepths, relative_counts, errors = [], [], []
-	# count number of AGN in same depth bins for each color bin, compare to counts for whole sample
-	for i in range(1, nbins+1):
-		binnedtab = tab[np.where(tab['bin'] == i)]
-
-
-
-		lons, lats = healpixhelper.equatorial_to_galactic(binnedtab['RA'], binnedtab['DEC'])
-
-		pix = hp.ang2pix(nside, lons, lats, lonlat=True)
-		depthsforpix = depth_map[pix]
-
-		shallowbintab = binnedtab[np.where(depthsforpix < 1500)]
-
-		deepbintab = binnedtab[np.where(depthsforpix > 1500)]
-
-		deepnormfactor, shallownormfactor = len(binnedtab) / 10 * deepfrac, len(binnedtab) / 10 * shallowfrac
-
-		shallowbinlon, shallowbinlat = coord_transforms.sky_transform(shallowbintab['RA'], shallowbintab['DEC'],
-		                                                              ['C', 'E'])
-		deepbinlon, deepbinlat = coord_transforms.sky_transform(deepbintab['RA'], deepbintab['DEC'], ['C', 'E'])
-
-		deepbincounts = np.bincount(np.digitize(deepbinlat, deepcoordbins))[1:]
-		shallowbincounts = np.bincount(np.digitize(shallowbinlat, shallowcoordbins))[1:]
-
-		shallowcorrections, deepcorrections = shallowbincounts / shallownormfactor, deepbincounts / deepnormfactor
-
-
-		#interpolator = interpolate.interp2d(x=med_depths, y=[shallowbincenters, deepbincenters], z=ratio_matrix,
-		#                                    fill_value=None)
-
-
-		#shallowweights, deepweights = np.ones(len(randtab_in_shallow_depth)), np.ones(len(randtab_in_deep))
-		shallowinterpolator = interpolate.interp1d(shallowbincenters, shallowcorrections, fill_value='extrapolate')
-		deepinterpolator = interpolate.interp1d(deepbincenters, deepcorrections, fill_value='extrapolate')
-		shallowweights = shallowinterpolator(shallowrandvals.value)
-		deepweights = deepinterpolator(deeprandvals.value)
-		#shallowweights = shallowcorrections[np.digitize(shallowrandvals.value, bins=shallowcoordbins) - 1]
-
-
-		#deepweights = deepcorrections[np.digitize(deeprandvals.value, bins=deepcoordbins) - 1]
-		randtab_in_shallow_depth['weight'] = shallowweights
-		randtab_in_deep['weight'] = deepweights
-		fullweighted_randtab = vstack([randtab_in_shallow_depth, randtab_in_deep])
-
-		normed_weights = fullweighted_randtab['weight'] / np.sum(fullweighted_randtab['weight'])
-
-		subsampled_rands = fullweighted_randtab[np.random.choice(np.arange(len(fullweighted_randtab)),
-		                                                         20*len(binnedtab), replace=False, p=normed_weights)]
-
-		subsampled_rands.write('catalogs/derived/catwise_randoms_%s.fits' % i, format='fits', overwrite=True)
-
-
-
-
-
-
-
-		"""factor = len(binnedtab) / float(len(tab))
-
-		counts = np.bincount(np.digitize(depthsforpix, depthbinedges))[1:]
-
-
-
-		relative_counts.append(factor * fullcounts / counts)
-
-		errors.append(np.sqrt(np.square(factor / counts * np.sqrt(fullcounts)) +
-		        np.square(factor * fullcounts / np.square(counts) * np.sqrt(counts))))"""
-	"""avgdepth = np.array([500, 3000])
-	plotting.depth_v_density(avgdepth, relative_counts, errors)
-	return relative_counts
-
-def correct_systematics(mode='weights'):
-	if mode == 'weights':
-		return
-	else:
-		randcat = Table.read('catalogs/derived/ls_randoms_1_filtered.fits')
-
-		rel_counts = correct_for_depth()
-		for j in range(len(rel_counts)):
-			init_weights = np.ones(len(randcat))
-			init_weights[np.where(randcat['PSFDEPTH_R'] < 1500)] = (rel_counts[j][0])**10
-			init_weights[np.where(randcat['PSFDEPTH_R'] > 1500)] = (rel_counts[j][1])**10
-			init_weights = init_weights / np.sum(init_weights)
-			sampleidxs = np.random.choice(np.arange(len(init_weights)), size=int(len(randcat) / 3), replace=False,
-			                              p=init_weights)
-			subsampledcat = randcat[sampleidxs]
-			#subsampledcat.write('catalogs/derived/ls_randoms_filtered_%s.fits' % (j + 1), format='fits',
-			# overwrite=True)
-
-
-			latcenters, ratios = density_by_coord('catwise', j+1, subsampledcat, 'E', 'lat', 10)
-			randelon, randelat = coord_transforms.sky_transform(subsampledcat['RA'], subsampledcat['DEC'],
-			                                                    trans=['C', 'E'])
-			interpfunc = interpolate.interp1d(latcenters, ratios, fill_value='extrapolate')
-			newweights = interpfunc(randelat)
-			newweights = newweights / np.sum(newweights)
-			sampleidxs = np.random.choice(np.arange(len(subsampledcat)), size=int(len(randcat) / 5), replace=False,
-			                              p=newweights)
-			subsampledcat = subsampledcat[sampleidxs]
-			subsampledcat.write('catalogs/derived/ls_randoms_filtered_%s.fits' % (j + 1), format='fits',
-			                    overwrite=True)
-
-			density_by_coord('catwise', j+1, subsampledcat, 'E', 'lat', 10)"""
 
 
 
